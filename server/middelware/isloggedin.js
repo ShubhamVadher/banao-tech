74a3200151b0987d91ab36c30a82c5ffceed9a5c
ShubhamVadher <vadhershubhambhai@gmail.com>
2026-02-16 12:19:43 +0530

    Update isloggedin.js

--- server/middelware/isloggedin.js
+++ server/middelware/isloggedin.js
@@ -1,25 +1,29 @@
-const User=require('../models/user');
-const jwt=require('jsonwebtoken');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
-const key=process.env.JWT
-const isloggedin=async(req,res,next)=>{
-    try{
-        if(!req.cookies.token||req.cookies.token==''){
-        
-        return res.status(500).json({message:"You must login first"});
-    }
-    const data=jwt.verify(req.cookies.token,key);
-    const user=await User.findOne({email:data.email});
-    if(!user){
-            window.alert('please login first');
-            return res.status(500).json({message:"You must login first"});
+const key = process.env.JWT;
+
+const isloggedin = async (req, res, next) => {
+    try {
+        if (!req.cookies || !req.cookies.token) {
+            return res.status(401).json({ message: "You must login first" });
         }
-        req.user=user;
-        next();
-    }
-    catch(err){
-        console.log(err);
-    }
 
-}
-module.exports=isloggedin;
+        const data = jwt.verify(req.cookies.token, key);
+
+        const user = await User.findOne({ email: data.email });
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = user;
+        next();
+
+    } catch (err) {
+        console.log("Auth Error:", err.message);
+        return res.status(401).json({ message: "Invalid or expired token" });
+    }
+};
+
+module.exports = isloggedin;
